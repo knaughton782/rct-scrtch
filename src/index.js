98f8c75b2ce2f4b60cd2847b49e83fbb50ae952b
Kirsten Naughton <kirsten.naughton@gmail.com>
2019-09-06 12:12:19 -0700

    setting up for the tvm makeover

--- src/index.js
+++ src/index.js
@@ -1,41 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import './style.css';
-
-class App extends React.Component {
-    state = {
-        counter: 0
-    }
-
-    increaseCounter = () => {
-        this.setState({ counter: this.state.counter + 1 })
-    }
-
-    decreaseCounter = () => {
-        this.setState({ counter: this.state.counter - 1 })
-    }
-
-
-    render() {
-        return <div>
-
-            <h1>{this.state.counter}</h1>
-            <button onClick={this.decreaseCounter}>
-                -
-            </button>
-            <button onClick={this.increaseCounter}>
-                +
-            </button>
-
-        </div>
-    }
-}
-
-
-
-
-
-
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
