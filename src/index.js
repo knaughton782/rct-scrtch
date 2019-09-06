21253904d794d38a355695392fc1b41c895b0911
Kirsten Naughton <kirsten.naughton@gmail.com>
2019-09-05 22:38:10 -0700

    updated for personalize and counter functions working

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import './style.css';
+
+class App extends React.Component {
+    state = {
+        counter: 0
+    }
+
+    increaseCounter = () => {
+        this.setState({ counter: this.state.counter + 1 })
+    }
+
+    decreaseCounter = () => {
+        this.setState({ counter: this.state.counter - 1 })
+    }
+
+
+    render() {
+        return <div>
+
+            <h1>{this.state.counter}</h1>
+            <button onClick={this.decreaseCounter}>
+                -
+            </button>
+            <button onClick={this.increaseCounter}>
+                +
+            </button>
+
+        </div>
+    }
+}
+
+
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
